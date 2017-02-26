5582295c307da3ef3355513170b4a787c98d14fc
Chad Van Klompenburg <chad.vanklompenburg@appropos.com>
2017-02-26 09:17:31 -0500

    push fixes

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+#NOW=$(date +"%Y-%m-%d-%H-%M-%S")
 REPOSITORY="474134703240.dkr.ecr.us-east-1.amazonaws.com/ecs-demo"
-TAG="${REPOSITORY}:ecs-demo_${NOW}"
-AWS_REGION="us-east-1"
+#TAG="${REPOSITORY}:ecs-demo_${NOW}"
+
+generate_tag(){
+    NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+    echo "${REPOSITORY}:ecs-demo_${NOW}"
+}
 
 show_menu(){
     PS3="What you want me do? : "
-    options=("Build Image" "Push Image" "Quit")
+    options=("Build/Push Image" "Quit")
     select opt in "${options[@]}"; do
         case $opt in
-            "Build Image")
-                (cd $DIR/../image && docker build -t $TAG .)
-                break
-                ;;
-            "Push Image")
-                (aws ecr get-login --region $AWS_REGION | bash)
-                (docker push $TAG)
+            "Build/Push Image")
+                (
+                    TAG=$(generate_tag)
+                    cd $DIR/../image && docker build -t $TAG .
+                    aws ecr get-login | bash
+                    docker push $TAG
+                )
                 break
                 ;;
             "Quit")
