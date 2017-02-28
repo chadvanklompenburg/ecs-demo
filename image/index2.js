340d8d6cf799b453ee76ce8eae12ba972eecafe9
Chad Van Klompenburg <chad.vanklompenburg@appropos.com>
2017-02-28 08:33:52 -0500

    small tweaks

--- image/index2.js
+++ image/index2.js
@@ -2,9 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World from index2.js')
+  res.send('Hello World from index2.js');
+  console.log("index2.js Responding to / route");
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
